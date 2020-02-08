a576ba922d644aef20ae6ff57ad73aa07a1e3b2f
Lutif <lmandhro@gmail.com>
2020-02-08 19:14:22 +0500

    code refactoring

--- routes/api/links/getLinks.js
+++ routes/api/links/getLinks.js
@@ -2,8 +2,11 @@ const getLinksRouter = require('express').Router()
 const Links =require('../../../database/models/Links')
 
 module.exports = getLinksRouter.get('/api/links', function (req, res, err) {
+    console.log('in server get links route before requesting db')
     Links.find({}, (err, linksList) => {
-    res.send(JSON.stringify(linksList))
+        console.log('in server get links route before requesting db')
+        res.send(JSON.stringify(linksList))
+        
     })
     
 })
